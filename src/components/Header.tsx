33d4b32225766b93262d1aa7065f7c7629e0c545
Guust Taillieu <guust@taillieusauw.net>
2023-10-21 16:00:26 +0200

    Made it mobile friendly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,78 +1,81 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { SocialIcon } from 'react-social-icons';
-import config from '../../tailwind.config';
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { SocialIcon } from "react-social-icons";
+import config from "../../tailwind.config";
 
 const socials = [
-	'https://github.com/GuustTaillieu/',
-	'https://www.linkedin.com/in/guust-taillieu-1b1b3a1b1/',
+  "https://github.com/GuustTaillieu/",
+  "https://www.linkedin.com/in/guust-taillieu-1b1b3a1b1/",
 ];
 
-const email = 'zita@gmail.com';
+const email = "zita@gmail.com";
 
 type Props = {};
 
 const Header = (props: Props) => {
-	return (
-		<motion.header
-			className='mx-auto grid grid-cols-2 md:grid-cols-3 text-center w-full px-4 fixed top-0 items-center z-50 md:px-20'
-			layoutId='navigation'
-			transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}>
-			<div className='flex justify-start'>
-				<motion.div
-					initial={{ opacity: 0, x: -400 }}
-					animate={{ opacity: 1, x: 0 }}
-					transition={{
-						duration: 1.2,
-						delay: 0.8,
-						ease: 'anticipate',
-					}}
-					className='space-x-2 hidden md:flex'>
-					{socials?.map((social) => (
-						<SocialIcon
-							url={social}
-							key={social}
-							target='_blank'
-							fgColor={
-								((config.theme?.extend?.colors as any)
-									?.dark as string) ?? 'black'
-							}
-							bgColor='transparent'
-							className='cursor-pointer'
-						/>
-					))}
-				</motion.div>
-			</div>
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ duration: 1.2, delay: 0.8, ease: 'anticipate' }}
-				className='flex justify-start md:justify-center'>
-				<h1 className='font-semibold sm:text-nav text-dark font-secondary uppercase p-4 text-lg'>
-					<Link href='/'>Zita Worm</Link>
-				</h1>
-			</motion.div>
-			<motion.div
-				initial={{ opacity: 0, x: 400 }}
-				animate={{ opacity: 1, x: 0 }}
-				transition={{ duration: 1.2, delay: 0.8, ease: 'anticipate' }}>
-				<h2 className='font-semibold sm:text-nav text-dark font-secondary uppercase text-lg flex-row items-center flex justify-end'>
-					<SocialIcon
-						url={'mailto:' + email}
-						target='_blank'
-						fgColor={
-							((config.theme?.extend?.colors as any)
-								?.dark as string) ?? 'black'
-						}
-						bgColor='transparent'
-						className='cursor-pointer'
-					/>
-					<span className='p-4 pl-0'>Get in touch</span>
-				</h2>
-			</motion.div>
-		</motion.header>
-	);
+  return (
+    <motion.header
+      className="fixed top-0 z-50 mx-auto grid w-full grid-cols-2 items-center bg-light px-4 text-center text-sm shadow-md sm:text-lg md:grid-cols-3 md:px-20 lg:bg-transparent lg:shadow-none"
+      layoutId="navigation"
+      transition={{ duration: 1, ease: "easeInOut", delay: 1 }}
+    >
+      <div className="hidden justify-start md:flex">
+        <motion.div
+          initial={{ opacity: 0, x: -400 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 1.2,
+            delay: 0.8,
+            ease: "anticipate",
+          }}
+          className="space-x-2"
+        >
+          {socials?.map((social) => (
+            <SocialIcon
+              url={social}
+              key={social}
+              target="_blank"
+              fgColor={
+                ((config.theme?.extend?.colors as any)?.dark as string) ??
+                "black"
+              }
+              bgColor="transparent"
+              className="cursor-pointer"
+            />
+          ))}
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, delay: 0.8, ease: "anticipate" }}
+        className="flex justify-start md:justify-center"
+      >
+        <h1 className="p-4 font-secondary font-semibold uppercase text-dark sm:text-nav">
+          <Link href="/">Zita Worm</Link>
+        </h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 400 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.2, delay: 0.8, ease: "anticipate" }}
+      >
+        <h2 className="flex flex-row items-center justify-end font-secondary font-semibold uppercase text-dark sm:text-nav">
+          <SocialIcon
+            url={"mailto:" + email}
+            target="_blank"
+            fgColor={
+              ((config.theme?.extend?.colors as any)?.dark as string) ?? "black"
+            }
+            bgColor="transparent"
+            className="cursor-pointer p-2"
+          />
+          <span className="p-4 pl-0">Get in touch</span>
+        </h2>
+      </motion.div>
+    </motion.header>
+  );
 };
 
 export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,18 @@ import { useRef } from "react";
 import Link from "next/link";
 import Page from "./Page";
 import HomeSection from "@/components/HomeSection";
+import { useScroll, useTransform, motion } from "framer-motion";
+import HomeTitle from "@/components/HomeTitle";
 
 export default function Home() {
   const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    container,
+    offset: ["-100%", "100%"],
+  });
+
+  const yPos = useTransform(scrollYProgress, [0, 1], ["105%", "-105%"]);
 
   return (
     <Page>
@@ -13,30 +22,32 @@ export default function Home() {
         className="h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-light py-24 font-default text-dark"
       >
         {/* ABOUT ME */}
-        <HomeSection index={1} container={container} />
+        <HomeSection index={1} container={container} title="About me" />
 
         {/* EXPERIENCE */}
-        <HomeSection index={2} container={container} />
+        <HomeSection index={2} container={container} title="Experience" />
 
         {/* SKILLS */}
-        <HomeSection index={3} container={container} />
+        <HomeSection index={3} container={container} title="Skills" />
 
         {/* PROJECTS */}
-        <HomeSection index={4} container={container} />
+        <HomeSection index={4} container={container} title="Projects" />
 
         {/* CONTACT */}
-        <HomeSection index={5} container={container} />
+        <HomeSection index={5} container={container} title="Contact" />
 
-        <div className="fixed left-60 top-1/2">
-          <Link href="/about">About</Link>
+        <div className="fixed left-16 top-1/2 hidden h-[3.75rem] overflow-hidden md:block lg:h-24 xl:left-32 2xl:left-60">
+          <motion.div className="flex flex-col" style={{ translateY: yPos }}>
+            <HomeTitle title="About me" link="/about" />
 
-          <Link href="/experience">Experience</Link>
+            <HomeTitle title="Experience" link="/experience" />
 
-          <Link href="/skills">Skills</Link>
+            <HomeTitle title="Skills" link="/skills" />
 
-          <Link href="/projects">Projects</Link>
+            <HomeTitle title="Projects" link="/projects" />
 
-          <Link href="/contact">Contact</Link>
+            <HomeTitle title="Contact" link="/contact" />
+          </motion.div>
         </div>
       </div>
     </Page>

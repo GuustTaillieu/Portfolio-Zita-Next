8a76160f5a66384f79b1e06133c737de1f4f895c
Guust Taillieu <guust@taillieusauw.net>
2023-10-27 19:39:40 +0200

    Added stretching and rotating of the cursor

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -1,15 +1,23 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import {
+    animate,
+    motion,
+    transform,
+    useMotionValue,
+    useSpring,
+} from "framer-motion";
 
 const CURSOR_SPEED = 0.8;
 const STICKY_DISTANCE = 0.2;
+const MAX_STRETCH_FACTOR = 1.3;
 
 type Props = {
     stickyElement: React.RefObject<any>;
 };
 
 const CustomCursor = ({ stickyElement }: Props) => {
+    const cursorRef = React.useRef<HTMLDivElement>(null);
     const [isHovering, setIsHovering] = useState(false);
     const CURSOR_SIZE = isHovering ? 60 : 20;
 
@@ -27,6 +35,15 @@ const CustomCursor = ({ stickyElement }: Props) => {
         x: useSpring(mouse.x, springOptions),
         y: useSpring(mouse.y, springOptions),
     };
+    const scale = {
+        x: useMotionValue(1),
+        y: useMotionValue(1),
+    };
+
+    const rotate = (distance: { x: number; y: number }) => {
+        const angle = Math.atan2(distance.y, distance.x);
+        animate(cursorRef.current!, { rotate: `${angle}rad` }, { duration: 0 });
+    };
 
     const handleMouseMove = (e: MouseEvent) => {
         const { clientX, clientY } = e;
@@ -34,13 +51,34 @@ const CustomCursor = ({ stickyElement }: Props) => {
             stickyElement.current.getBoundingClientRect();
         const center = { x: left + width / 2, y: top + height / 2 };
         const distance = { x: clientX - center.x, y: clientY - center.y };
+
         if (isHovering) {
+            // ROTATION
+            rotate(distance);
+
             mouse.x.set(
                 center.x - CURSOR_SIZE / 2 + distance.x * STICKY_DISTANCE,
             );
             mouse.y.set(
                 center.y - CURSOR_SIZE / 2 + distance.y * STICKY_DISTANCE,
             );
+            // STRETCH
+            const absoluteDistance = Math.max(
+                Math.abs(distance.x),
+                Math.abs(distance.y),
+            );
+            const nexScaleX = transform(
+                absoluteDistance,
+                [0, width / 2],
+                [1, MAX_STRETCH_FACTOR],
+            );
+            const nexScaleY = transform(
+                absoluteDistance,
+                [0, height / 2],
+                [1, MAX_STRETCH_FACTOR * 0.7],
+            );
+            scale.x.set(nexScaleX);
+            scale.y.set(nexScaleY);
         } else {
             mouse.x.set(clientX - CURSOR_SIZE / 2);
             mouse.y.set(clientY - CURSOR_SIZE / 2);
@@ -53,6 +91,11 @@ const CustomCursor = ({ stickyElement }: Props) => {
 
     const handleMouseLeave = () => {
         setIsHovering(false);
+        animate(
+            cursorRef.current!,
+            { scaleX: 1, scaleY: 1 },
+            { duration: 0.1, type: "spring" },
+        );
     };
 
     useEffect(() => {
@@ -72,13 +115,25 @@ const CustomCursor = ({ stickyElement }: Props) => {
         };
     });
 
+    type templateProps = {
+        rotate: string;
+        scaleX: number;
+        scaleY: number;
+    };
+    const template = ({ rotate, scaleX, scaleY }: templateProps) =>
+        `rotate(${rotate}) scaleX(${scaleX}) scaleY(${scaleY})`;
+
     return (
         <motion.div
+            transformTemplate={template}
             className="fixed aspect-square rounded-full bg-black"
+            ref={cursorRef}
             style={{
                 width: CURSOR_SIZE,
                 left: smoothMouse.x,
                 top: smoothMouse.y,
+                scaleX: scale.x,
+                scaleY: scale.y,
             }}
             animate={{ width: CURSOR_SIZE }}
         />
